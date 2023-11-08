5cbc311d6382e87e60f67c9d9d788b74068074b7
Dishpak <djdishpak@gmail.com>
2023-11-08 18:27:06 +0300

    fix subsribition form

--- src/components/contacts/ContactForm.jsx
+++ src/components/contacts/ContactForm.jsx
@@ -11,7 +11,7 @@ const ContactForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     handleInputsReset();
-    setShowModal(true)
+    setShowModal(true);
   }
 
   return (
--- src/components/mainPage/SubscribeSection.jsx
+++ src/components/mainPage/SubscribeSection.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import Container from "react-bootstrap/Container";
-import InputGroup from "react-bootstrap/InputGroup";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import React, {useState} from 'react';
+import {Container, InputGroup, Form, Button} from "react-bootstrap";
+import {useFormInputs} from "../../hooks/useFormInputs";
+import BootstrapModal from "../helpers/BootstrapModal";
 
 const SubscribeSection = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [formInputs, handleInputChange, handleInputsReset] = useFormInputs({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleInputsReset();
+    setShowModal(true);
+  }
   return (
     <section className="subscribe">
       <Container className="py-5 w-50">
         <h2>Stay in fashion loop!</h2>
         <p>Sign up for our newsletter to receive exclusive promotions, style updates, and early access to new arrivals.
           Join now and stay ahead in the world of fashion!</p>
-        <InputGroup className="w-50 mx-auto">
-          <Form.Control placeholder="enter your email"/>
-          <Button variant="danger">subscribe</Button>
-        </InputGroup>
+        <Form onSubmit={handleSubmit}><InputGroup className="w-50 mx-auto">
+          <Form.Control
+            placeholder="enter your email"
+            value={formInputs.email || ''}
+            onChange={handleInputChange}
+            name="email"
+            onSubmit={handleSubmit}
+          />
+          <Button variant="danger" type="submit">subscribe</Button>
+        </InputGroup></Form>
+        <BootstrapModal show={showModal} setShow={setShowModal} title="Thank you!">
+          <p>You've successfully subscribed!</p>
+        </BootstrapModal>
       </Container>
     </section>);
 };
